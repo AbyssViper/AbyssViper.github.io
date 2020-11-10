af1057b9e51a9defa51f01b05ac37549e802b7d3
AbyssViper <825793075@qq.com>
2020-11-10 22:30:29 +0800

    Update

--- kubernetes/handbook/en.search.min.4c830f7907a85b73ae3d4a29c79b2f45976c1f12f025b33172cdf41e85f99ade.js
+++ kubernetes/handbook/en.search.min.4c830f7907a85b73ae3d4a29c79b2f45976c1f12f025b33172cdf41e85f99ade.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/kubernetes/handbook/flexsearch.min.js');loadScript('/kubernetes/handbook/en.search-data.min.a72375424e3c31f25198f1fa2efb30b12c87f7d06cbad38dd6739f245e67b519.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/kubernetes/handbook/flexsearch.min.js');loadScript('/kubernetes/handbook/en.search-data.min.f9f9fabdbac8062051f3bd53bf59e278cecbf9cf1a038ee903625c3853c0c38f.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
